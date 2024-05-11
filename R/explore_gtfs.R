00fa17b25966e0ed40db44d45addc9f855aef32b
Gustavo Guimarães <49100132+gustavo234guimaraes@users.noreply.github.com>
2024-05-10 21:22:34 -0300

    Add files via upload

--- R/explore_gtfs.R
+++ R/explore_gtfs.R
@@ -1,29 +1,19 @@
-explore_gtfs <- 
-#explore_gtfs_dev <-
-  function(gtfs){
+
+
+exploreGTFS <- function(gtfs){
   
   ui <- shiny::navbarPage(
-    h1('GTFS Exploration Dashboard'),
-    title = "GTFS wizard",
+    title = "Explore GTFS",
     tabPanel(
       'Overview',
       column(
         width = 7,
-        leafletOutput('overview_map1',height = '75vh'),
+        leafletOutput('overview_map',height = '75vh'),
         tags$style(
-          'div#overview_map1{
+          'div#overview_map{
           width:100%;
           heigth:60vh;
-          border:solid green;
-          border-radius:10px;
-          }'
-        ),
-        leafletOutput('overview_map2',height = '75vh'),
-        tags$style(
-          'div#overview_map2{
-          width:100%;
-          heigth:60vh;
-          border:solid green;
+          border:solid orange;
           border-radius:10px;
           }'
         )
@@ -31,7 +21,7 @@ explore_gtfs <-
       ),
       column(
         width = 5,
-        plotlyOutput('freq.sparkline')
+        plotlyOutput('overview_plot_freq')
       )
     ),
     tabPanel('By Route')
@@ -39,99 +29,21 @@ explore_gtfs <-
   
   server <- function(input, output, session) {
     
-    trips.shp <- 
-      tidytransit::shapes_as_sf(gtfs$shapes)
-    
-    trips.shp <-
-      trips.shp %>% 
-      dplyr::bind_cols(.,
-                       `# trips` = st_intersects(trips.shp, trips.shp) %>% 
-                         lapply(., length) %>% 
-                         unlist()
-      )
-    
-    output$overview_map1 <- renderLeaflet({
-      leaflet::leaflet() %>%
-        leaflet::addTiles(group = "OSM") %>% 
-        leaflet::addProviderTiles(providers$CartoDB.Positron,group = 'Carto-Light') %>% 
-        leaflet::addProviderTiles(providers$CartoDB.DarkMatter, group = 'Carto - Dark') %>% 
-        leaflet::addLayersControl(baseGroups = c('Carto - Light','Carto - Dark','OSM')) %>% 
-        leaflet::addPolylines(data = trips.shp) %>%
-        leaflet.extras::addFullscreenControl() %>% 
-        leaflet.extras::addResetMapButton() %>% 
-        leaflet.extras::addControlGPS() %>% 
-        leaflet.extras::addSearchOSM()
-      
+    output$overview_map <- renderLeaflet({
+      leaflet() %>% 
+        addPolylines(data = geom_shapes(gtfs$shapes)) %>% 
+        addTiles()
     })
     
-    stops.shp <- 
-      tidytransit::gtfs_as_sf(gtfs) %>% 
-      .$stops %>% 
-      dplyr::left_join(
-        gtfs$stop_times %>%
-          group_by(stop_id) %>%
-          reframe(`# trips` = n())
-      )
-    
-    output$overview_map2 <- renderLeaflet({
-      leaflet::leaflet() %>% 
-        addTiles(group = "OSM") %>% 
-        addProviderTiles(providers$CartoDB.Positron,group = 'Carto-Light') %>% 
-        addProviderTiles(providers$CartoDB.DarkMatter, group = 'Carto - Dark') %>% 
-        addLayersControl(baseGroups = c('Carto - Light','Carto - Dark','OSM')) %>% 
-        leaflet::addAwesomeMarkers(data = stops.shp,
-                                   popup = ~paste0('# trips ', `# trips`),
-                                   clusterOptions = markerClusterOptions()
-        ) %>% 
-        leaflet.extras::addFullscreenControl() %>% 
-        leaflet.extras::addResetMapButton() %>% 
-        leaflet.extras::addControlGPS() %>% 
-        leaflet.extras::addSearchOSM()
-    })
-    
-    output$freq.sparkline <- renderPlotly({
-      p <- 
-        gtfs$stop_times %>% 
-        dplyr::group_by(trip_id) %>% 
-        dplyr::reframe(departure = arrival_time[1]) %>% 
-        dplyr::left_join(
-          gtfs$trips %>% 
-            dplyr::select(route_id, service_id, trip_id, shape_id)
-        ) %>% 
-        # filter(route_id %in% c() & service_id %in% c()) # filtrar por dia e por rota
-        dplyr::mutate(hour = str_extract(departure, '\\d+') %>% as.numeric()) %>% 
-        #group_by(route_id, hour, shape_id, service_id) %>% 
-        dplyr::group_by(route_id, hour) %>% 
-        dplyr::reframe(frequency = n()) %>% 
-        dplyr::arrange(hour) %>%  
-        tidyr::pivot_wider(., names_from = hour, values_from = frequency, values_fill = 0) %>% 
-        dplyr::arrange(route_id) %>%
-        tidyr::pivot_longer(., cols = 2:ncol(.), names_to = "hour", values_to = "frequency") %>% 
-        dplyr::mutate(hour = as.numeric(hour)) %>% #.[1:300, ] %>% 
-        ggplot() +
-        geom_vline(color = 'red', xintercept = c(0, 6, 12, 18, 24)) +
-        geom_line(aes(hour, frequency), linewidth = 1) +
-        facet_grid(route_id~.,
-                   #scales = 'free_y'
-        ) +
-        theme_linedraw() +
-        scale_x_continuous(breaks = c(0, 6, 12, 18, 24)) +
-        theme(legend.position = 'none',
-              axis.title = element_blank(),
-              axis.text.y = element_blank(),
-              panel.grid.major.x = element_blank(),
-              panel.grid.major.y = element_blank(),
-              axis.ticks = element_blank()
-        )
-      
-      plotly::ggplotly(p, height = 50 * length(unique(gtfs$routes$route_id)))
-      
-    })
     
   }
   
   return(shiny::shinyApp(ui, server))
   
+  
 }
 
-#explore_gtfs_dev(gtfs)
+exploreGTFS(gtfs_list)
+
+
+
