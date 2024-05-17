e715574385e9ba366fd3e74e82f4c0a71858042a
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-05-16 21:52:04 -0300

    Add files via upload

---
+++ R/get_dwelltime.R
@@ -0,0 +1,35 @@
+get_dwelltime <- function(gtfs){
+  
+  dwell_time <- 
+    gtfs$stop_times %>% 
+    dplyr::select(arrival_time, departure_time, stop_id, trip_id)  %>% 
+    dplyr::mutate(stop_id = as_factor(stop_id)) %>% 
+    dplyr::filter(!arrival_time == '' & !departure_time == "") %>% 
+    dplyr::mutate(hour = str_extract(arrival_time, "\\d+") %>% as.numeric(),
+                  arrival_time = gtfs$stop_times$arrival_time %>% 
+                    stringr::str_split(":") %>% 
+                    lapply(FUN = as.numeric) %>% 
+                    lapply(FUN = function(x){
+                      x[1]*60*60+x[2]*60+x[3]
+                    }) %>% 
+                    unlist() %>% 
+                    na.omit(),
+                  departure_time = gtfs$stop_times$departure_time %>% 
+                    stringr::str_split(":") %>% 
+                    lapply(FUN = as.numeric) %>% 
+                    lapply(FUN = function(x){
+                      x[1]*60*60+x[2]*60+x[3]
+                    }) %>% 
+                    unlist() %>% 
+                    na.omit(),
+                  dwell_time = departure_time - arrival_time
+    ) %>% 
+    dplyr::filter(dwell_time <= 300) %>% # máximo 5 minutos
+    dplyr::left_join(gtfs$trips) %>% 
+    dplyr::select(route_id, stop_id, hour, dwell_time)
+  
+  return(dwell_time)
+  
+}
+
+#get_dwelltime(gtfs)
