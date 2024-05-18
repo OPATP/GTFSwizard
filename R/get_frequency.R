e4c917caeb64a6379e6ca95d44c25e25b48c90b2
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-05-18 16:03:45 -0300

    Add files via upload

--- R/get_frequency.R
+++ R/get_frequency.R
@@ -5,14 +5,21 @@ get_frequency <- function(gtfs){
     dplyr::reframe(departure = arrival_time[1]) %>% 
     dplyr::left_join(
       gtfs$trips %>% 
-        dplyr::select(route_id, service_id, trip_id, shape_id)
+        dplyr::select(route_id, service_id, trip_id)
     ) %>% 
     # filter(route_id %in% c() & service_id %in% c()) # filtrar por dia e por rota
     dplyr::mutate(hour = str_extract(as.character(departure), '\\d+') %>% as.numeric()) %>% 
-    dplyr::group_by(route_id, hour) %>% 
-    dplyr::reframe(frequency = n())
+    dplyr::group_by(route_id, hour, service_id) %>% 
+    dplyr::reframe(frequency = n()) %>% 
+    left_join(
+      gtfs$dates_services$service_id %>%
+        unlist %>%
+        table %>%
+        as_tibble %>% 
+        stats::setNames(c('service_id', 'service_frequency'))
+    )
   
   return(freq)
 }
 
-#get_frequency(gtfs)
+get_frequency(gtfs)
