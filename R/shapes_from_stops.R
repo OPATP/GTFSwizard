d414bf789b770138238186d57b66ccb326b1098a
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-05-24 16:16:55 -0300

    Add files via upload

--- R/shapes_from_stops.R
+++ R/shapes_from_stops.R
@@ -1,6 +1,6 @@
 shapes_from_stops <- function(gtfs){
   
-  warning('This algorithm reconstructs the shape files using a Euclidean approximation, based on the coordinates and sequence of stops for each trip.')
+  warning('This algorithm reconstructs the shape files using an Euclidean approximation, based on the coordinates and sequence of stops for each trip.')
   
   if(!"wizardgtfs" %in% class(gtfs))(gtfs <- GTFSwizard::gtfs_to_wizard(gtfs))
   
@@ -18,21 +18,21 @@ shapes_from_stops <- function(gtfs){
     sf::st_as_sf(crs = 4326) %>% 
     sf::st_cast('LINESTRING') %>% 
     dplyr::left_join(gtfs$trips %>%
-                       select(trip_id, route_id)) %>% 
+                       dplyr::select(trip_id, route_id)) %>% 
     dplyr::group_by(geometry) %>% 
     dplyr::reframe(trip_id = list(trip_id)) %>%
     dplyr::mutate(shape_id = paste0('shape-', 1:n()))
   
   gtfs$shapes <-
     shapes.dic[, c(3, 1)] %>% 
-    st_as_sf()
+    sf::st_as_sf()
   
   gtfs$trips <- 
     gtfs$trips %>% 
-    select(-shape_id) %>% 
-    left_join(shapes.dic %>% 
-                select(-geometry) %>% 
-                tidyr::unnest(cols = 'trip_id'))
+    dplyr::select(-shape_id) %>% 
+    dplyr::left_join(shapes.dic %>%
+                       dplyr::select(-geometry) %>% 
+                       tidyr::unnest(cols = 'trip_id'))
   
   gtfs <-
     tidytransit::sf_as_tbl(gtfs)
