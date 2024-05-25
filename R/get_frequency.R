a94b9e419292b6b6a7f776b2275c3d041e07b89b
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-05-25 10:14:12 -0300

    Add files via upload

--- R/get_frequency.R
+++ R/get_frequency.R
@@ -1,4 +1,6 @@
-get_frequency <- function(gtfs){
+get_frequency <- function(gtfs, simplify = T){
+  
+  get_frequency_byroute <- function(gtfs){
   
   if(!"wizardgtfs" %in% class(gtfs))(gtfs <- GTFSwizard::gtfs_to_wizard(gtfs))
   
@@ -11,7 +13,7 @@ get_frequency <- function(gtfs){
     tibble(service_id = .) %>% 
     dplyr::left_join(service_pattern) %>% 
     dplyr::group_by(service_pattern) %>% 
-    reframe(pattern_frequency= n())
+    dplyr::reframe(pattern_frequency= n())
   
   freq <-
     gtfs$stop_times %>% 
@@ -20,16 +22,68 @@ get_frequency <- function(gtfs){
     dplyr::left_join(gtfs$trips %>% 
                        dplyr::select(route_id, service_id, trip_id),
                      by = 'trip_id') %>% 
-    dplyr::left_join(service_pattern, by = 'service_id') %>% 
-    dplyr::mutate(hour = str_extract(as.character(departure), '\\d+') %>% as.numeric()) %>% 
-    dplyr::group_by(route_id, hour, service_pattern) %>% 
+    dplyr::left_join(service_pattern, by = 'service_id') %>% # aqui
+    #dplyr::mutate(hour = str_extract(as.character(departure), '\\d+') %>% as.numeric()) %>% 
+    dplyr::group_by(route_id, service_pattern) %>% # aqui
     dplyr::reframe(frequency = n()) %>% 
-    dplyr::left_join(service_pattern_freq,
+    dplyr::left_join(service_pattern_freq, # aqui
+                     by = 'service_pattern') %>%
+    #filter(route_id %in% c() & service_id %in% c()) # filtrar por dia e por rota
+    dplyr::select(route_id, frequency, service_pattern, pattern_frequency)
+  
+  return(freq)
+  
+}
+
+  get_frequency_detailed <- function(gtfs){
+  
+  if(!"wizardgtfs" %in% class(gtfs))(gtfs <- GTFSwizard::gtfs_to_wizard(gtfs))
+  
+  service_pattern <- 
+    GTFSwizard::get_servicepattern(gtfs)
+  
+  service_pattern_freq <- 
+    gtfs$dates_services$service_id %>% 
+    unlist %>%
+    tibble(service_id = .) %>% 
+    dplyr::left_join(service_pattern) %>% 
+    dplyr::group_by(service_pattern) %>% 
+    dplyr::reframe(pattern_frequency= n())
+  
+  freq <-
+    gtfs$stop_times %>% 
+    dplyr::group_by(trip_id) %>% 
+    dplyr::reframe(departure = arrival_time[1]) %>% 
+    dplyr::left_join(gtfs$trips %>% 
+                       dplyr::select(route_id, service_id, trip_id),
+                     by = 'trip_id') %>% 
+    dplyr::left_join(service_pattern, by = 'service_id') %>% # aqui
+    dplyr::mutate(hour = str_extract(as.character(departure), '\\d+') %>% as.numeric()) %>% 
+    dplyr::group_by(route_id, hour, service_pattern) %>% # aqui
+    dplyr::reframe(frequency = n()) %>% 
+    dplyr::left_join(service_pattern_freq, # aqui
                      by = 'service_pattern') %>%
     #filter(route_id %in% c() & service_id %in% c()) # filtrar por dia e por rota
     dplyr::select(route_id, hour, frequency, service_pattern, pattern_frequency)
   
   return(freq)
+  
+  }
+  
+  if (simplify == T) {
+    hw <- get_frequency_byroute(gtfs)
+  }
+  
+  if (simplify == F) {
+    hw <- get_frequency_detailed(gtfs)
+  }
+  
+  if (!simplify %in% c(T, F)) {
+    hw <- warning('"simplify" should be one of TRUE or FALSE')
+  }
+  
+  return(hw)
+  
 }
 
-#get_frequency(gtfs)
+#get_frequency(gtfs, simplify = F)
