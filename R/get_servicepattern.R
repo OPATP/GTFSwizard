2db06f0bd126bede1c32e8441f3907ed18f5ea7e
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-05-24 22:14:50 -0300

    Add files via upload

---
+++ R/get_servicepattern.R
@@ -0,0 +1,22 @@
+get_servicepattern <- function(gtfs){
+  
+  if(!"wizardgtfs" %in% class(gtfs))(gtfs <- GTFSwizard::gtfs_to_wizard(gtfs))
+  
+  service_pattern <- 
+    gtfs$dates_services %>% 
+    group_by(service_id) %>% 
+    reframe(dates = list(as.character(date))) %>% 
+    mutate(service_pattern = paste0('servicepattern-', 1:n()))
+    
+  
+  while(is_list(service_pattern$service_id)) {
+    service_pattern <- service_pattern %>% unnest(service_id)
+  }
+  
+  while(is_list(service_pattern$dates)) {
+    service_pattern <- service_pattern %>% unnest(dates)
+  }
+  
+  return(service_pattern)
+  
+}
