ec0427a5ca16599ea6f92b967f0c674086748968
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-06-13 22:53:27 -0300

    Add files via upload

--- R/get_servicepattern.R
+++ R/get_servicepattern.R
@@ -7,14 +7,15 @@ get_servicepattern <- function(gtfs){
   
   service_pattern <- 
     gtfs$dates_services %>% 
-    group_by(service_id) %>% 
-    reframe(dates = list(as.character(date)),
-            pattern_frequency = n()) %>% 
-    mutate(service_pattern = paste0('servicepattern-', 1:n()) %>% as_factor()) %>% 
-    select(service_id, service_pattern, pattern_frequency)
+    dplyr::group_by(service_id) %>% 
+    dplyr::reframe(dates = list(as.character(date)),
+                   pattern_frequency = n()) %>% 
+    dplyr::arrange(-pattern_frequency) %>% 
+    dplyr::mutate(service_pattern = paste0('servicepattern-', 1:n()) %>% as_factor()) %>% 
+    dplyr::select(service_id, service_pattern, pattern_frequency)
   
   
-  while(is_list(service_pattern$service_id)) {
+  while(rlang::is_list(service_pattern$service_id)) {
     service_pattern <- service_pattern %>% unnest(., cols = c(service_id))
   }
   
