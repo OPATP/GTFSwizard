964362beed065640a139946d54c7bf67f2ae8432
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-09-03 15:33:47 -0300

    Add files via upload

---
+++ R/empty_gtfs.R
@@ -0,0 +1,13 @@
+empty_gtfs <- function(full = F) {
+  
+  checkmate::assert_logical(full)
+  
+  if(!full) {
+    message("Loading only required fields. To get required and optional fields please use 'full = TRUE'.")
+    return(GTFSwizard::empty_gtfs)
+  } else {
+    message("Loading required and optional fields. To get only required fields please use 'full = FALSE'.")
+    return(GTFSwizard::empty_gtfs_full)
+  }
+  
+}
