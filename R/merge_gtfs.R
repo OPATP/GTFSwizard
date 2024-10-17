25c794dcf5eb11bb9d29076ec3524b1ddd586cf4
Nelson Quesado <79152968+nelsonquesado@users.noreply.github.com>
2024-10-16 22:01:37 -0300

    Add files via upload

--- R/merge_gtfs.R
+++ R/merge_gtfs.R
@@ -1,6 +1,8 @@
-merge_gtfs <- function(gtfs.x, gtfs.y){
+merge_gtfs <- function(gtfs.x, gtfs.y, suffix = TRUE){
   
   # checa a classe ----
+  checkmate::assert_logical(suffix)
+  
   if(!"wizardgtfs" %in% class(gtfs.x)){
     gtfs <- GTFSwizard::as_wizardgtfs(gtfs.x)
     warning('\nThe first gtfs object is not of the wizardgtfs class.\nComputation may take longer.\nUsing as.gtfswizard() is advised.')
@@ -11,96 +13,100 @@ merge_gtfs <- function(gtfs.x, gtfs.y){
     warning('\nThe first gtfs object is not of the wizardgtfs class.\nComputation may take longer.\nUsing as.gtfswizard() is advised.')
   }
   
-  # ajusta os id's para evitar duplicados ----
-  gtfs.x$agency$agency_id <- paste0(gtfs.x$agency$agency_id, '.x')
-  gtfs.y$agency$agency_id <- paste0(gtfs.y$agency$agency_id, '.y')
   
-  gtfs.x$routes$agency_id <- paste0(gtfs.x$routes$agency_id, '.x')
-  gtfs.y$routes$agency_id <- paste0(gtfs.y$routes$agency_id, '.y')
-  
-  gtfs.x$routes$route_id <- paste0(gtfs.x$routes$route_id, '.x')
-  gtfs.y$routes$route_id <- paste0(gtfs.y$routes$route_id, '.y')
-  
-  gtfs.x$trips$route_id <- paste0(gtfs.x$trips$route_id, '.x')
-  gtfs.y$trips$route_id <- paste0(gtfs.y$trips$route_id, '.y')
-  
-  gtfs.x$trips$trip_id <- paste0(gtfs.x$trips$trip_id, '.x')
-  gtfs.y$trips$trip_id <- paste0(gtfs.y$trips$trip_id, '.y')
-  
-  gtfs.x$trips$shape_id <- paste0(gtfs.x$trips$shape_id, '.x')
-  gtfs.y$trips$shape_id <- paste0(gtfs.y$trips$shape_id, '.y')
-  
-  gtfs.x$trips$service_id <- paste0(gtfs.x$trips$service_id, '.x')
-  gtfs.y$trips$service_id <- paste0(gtfs.y$trips$service_id, '.y')
-  
-  gtfs.x$stop_times$trip_id <- paste0(gtfs.x$stop_times$trip_id, '.x')
-  gtfs.y$stop_times$trip_id <- paste0(gtfs.y$stop_times$trip_id, '.y')
-  
-  gtfs.x$stop_times$stop_id <- paste0(gtfs.x$stop_times$stop_id, '.x')
-  gtfs.y$stop_times$stop_id <- paste0(gtfs.y$stop_times$stop_id, '.y')
-  
-  gtfs.x$stops$stop_id <- paste0(gtfs.x$stops$stop_id, '.x')
-  gtfs.y$stops$stop_id <- paste0(gtfs.y$stops$stop_id, '.y')
-  
-  if(!is_null(gtfs.x$fare_rules$route_id)){
-    gtfs.x$fare_rules$route_id <- paste0(gtfs.x$fare_rules$route_id, '.x')
-    gtfs.x$fare_rules$fare_id <- paste0(gtfs.x$fare_rules$fare_id, '.x')
-  }
-  
-  if(!is_null(gtfs.y$fare_rules$route_id)){
-    gtfs.y$fare_rules$route_id <- paste0(gtfs.y$fare_rules$route_id, '.y')
-    gtfs.y$fare_rules$fare_id <- paste0(gtfs.y$fare_rules$fare_id, '.y')
-  }
-  
-  if(!is_null(gtfs.x$fare_attributes)){
-    gtfs.x$fare_attributes$fare_id <- paste0(gtfs.x$fare_attributes$fare_id, '.x')
-  }
-  
-  if(!is_null(gtfs.y$fare_attributes)){
-    gtfs.y$fare_attributes$fare_id <- paste0(gtfs.y$fare_attributes$fare_id, '.y')
-  }
-  
-  if(!is_null(gtfs.x$shapes)){
-    gtfs.x$shapes$shape_id <- paste0(gtfs.x$shapes$shape_id, '.x')
+  if (isTRUE(suffix)) { 
+    
+    gtfs.x$agency$agency_id <- paste0(gtfs.x$agency$agency_id, '.x')
+    gtfs.y$agency$agency_id <- paste0(gtfs.y$agency$agency_id, '.y')
+    
+    gtfs.x$routes$agency_id <- paste0(gtfs.x$routes$agency_id, '.x')
+    gtfs.y$routes$agency_id <- paste0(gtfs.y$routes$agency_id, '.y')
+    
+    gtfs.x$routes$route_id <- paste0(gtfs.x$routes$route_id, '.x')
+    gtfs.y$routes$route_id <- paste0(gtfs.y$routes$route_id, '.y')
+    
+    gtfs.x$trips$route_id <- paste0(gtfs.x$trips$route_id, '.x')
+    gtfs.y$trips$route_id <- paste0(gtfs.y$trips$route_id, '.y')
+    
+    gtfs.x$trips$trip_id <- paste0(gtfs.x$trips$trip_id, '.x')
+    gtfs.y$trips$trip_id <- paste0(gtfs.y$trips$trip_id, '.y')
+    
+    gtfs.x$trips$shape_id <- paste0(gtfs.x$trips$shape_id, '.x')
+    gtfs.y$trips$shape_id <- paste0(gtfs.y$trips$shape_id, '.y')
+    
+    gtfs.x$trips$service_id <- paste0(gtfs.x$trips$service_id, '.x')
+    gtfs.y$trips$service_id <- paste0(gtfs.y$trips$service_id, '.y')
+    
+    gtfs.x$stop_times$trip_id <- paste0(gtfs.x$stop_times$trip_id, '.x')
+    gtfs.y$stop_times$trip_id <- paste0(gtfs.y$stop_times$trip_id, '.y')
+    
+    gtfs.x$stop_times$stop_id <- paste0(gtfs.x$stop_times$stop_id, '.x')
+    gtfs.y$stop_times$stop_id <- paste0(gtfs.y$stop_times$stop_id, '.y')
+    
+    gtfs.x$stops$stop_id <- paste0(gtfs.x$stops$stop_id, '.x')
+    gtfs.y$stops$stop_id <- paste0(gtfs.y$stops$stop_id, '.y')
+    
+    if(!is_null(gtfs.x$fare_rules$route_id)){
+      gtfs.x$fare_rules$route_id <- paste0(gtfs.x$fare_rules$route_id, '.x')
+      gtfs.x$fare_rules$fare_id <- paste0(gtfs.x$fare_rules$fare_id, '.x')
+    }
+    
+    if(!is_null(gtfs.y$fare_rules$route_id)){
+      gtfs.y$fare_rules$route_id <- paste0(gtfs.y$fare_rules$route_id, '.y')
+      gtfs.y$fare_rules$fare_id <- paste0(gtfs.y$fare_rules$fare_id, '.y')
+    }
+    
+    if(!is_null(gtfs.x$fare_attributes)){
+      gtfs.x$fare_attributes$fare_id <- paste0(gtfs.x$fare_attributes$fare_id, '.x')
+    }
+    
+    if(!is_null(gtfs.y$fare_attributes)){
+      gtfs.y$fare_attributes$fare_id <- paste0(gtfs.y$fare_attributes$fare_id, '.y')
+    }
+    
+    if(!is_null(gtfs.x$shapes)){
+      gtfs.x$shapes$shape_id <- paste0(gtfs.x$shapes$shape_id, '.x')
+    }  
+    
+    if(!is_null(gtfs.y$shapes)){
+      gtfs.y$shapes$shape_id <- paste0(gtfs.y$shapes$shape_id, '.y')
+    }
+    
+    if(!is_null(gtfs.x$calendar)){
+      gtfs.x$calendar$service_id <- paste0(gtfs.x$calendar$service_id, '.x')
+    }
+    
+    if(!is_null(gtfs.y$calendar)){
+      gtfs.y$calendar$service_id <- paste0(gtfs.y$calendar$service_id, '.y')
+    }
+    
+    if(!is_null(gtfs.x$calendar_dates)){
+      gtfs.x$calendar_dates$service_id <- paste0(gtfs.x$calendar_dates$service_id, '.x')
+    }
+    
+    if(!is_null(gtfs.y$calendar_dates)){
+      gtfs.y$calendar_dates$service_id <- paste0(gtfs.y$calendar_dates$service_id, '.y')
+    }  
+    
+    if(!is_null(gtfs.x$frequencies)){
+      gtfs.x$frequencies$trip_id <- paste0(gtfs.x$frequencies$trip_id, '.x')
+    }
+    
+    if(!is_null(gtfs.y$frequencies)){
+      gtfs.y$frequencies$trip_id <- paste0(gtfs.y$frequencies$trip_id, '.y')
+    }
+    
+    if(!is_null(gtfs.x$transfers)){
+      gtfs.x$transfers$trip_id <- paste0(gtfs.x$transfers$trip_id, '.x')
+      gtfs.x$transfers$stop_id <- paste0(gtfs.x$transfers$stop_id, '.x')
+    }
+    
+    if(!is_null(gtfs.y$transfers)){
+      gtfs.y$transfers$trip_id <- paste0(gtfs.y$transfers$trip_id, '.y')
+      gtfs.y$transfers$stop_id <- paste0(gtfs.y$transfers$stop_id, '.y')
+    }
+    
   }  
-  
-  if(!is_null(gtfs.y$shapes)){
-    gtfs.y$shapes$shape_id <- paste0(gtfs.y$shapes$shape_id, '.y')
-  }
-  
-  if(!is_null(gtfs.x$calendar)){
-    gtfs.x$calendar$service_id <- paste0(gtfs.x$calendar$service_id, '.x')
-  }
-  
-  if(!is_null(gtfs.y$calendar)){
-    gtfs.y$calendar$service_id <- paste0(gtfs.y$calendar$service_id, '.y')
-  }
-  
-  if(!is_null(gtfs.x$calendar_dates)){
-    gtfs.x$calendar_dates$service_id <- paste0(gtfs.x$calendar_dates$service_id, '.x')
-  }
-  
-  if(!is_null(gtfs.y$calendar_dates)){
-    gtfs.y$calendar_dates$service_id <- paste0(gtfs.y$calendar_dates$service_id, '.y')
-  }  
-  
-  if(!is_null(gtfs.x$frequencies)){
-    gtfs.x$frequencies$trip_id <- paste0(gtfs.x$frequencies$trip_id, '.x')
-  }
-  
-  if(!is_null(gtfs.y$frequencies)){
-    gtfs.y$frequencies$trip_id <- paste0(gtfs.y$frequencies$trip_id, '.y')
-  }
-  
-  if(!is_null(gtfs.x$transfers)){
-    gtfs.x$transfers$trip_id <- paste0(gtfs.x$transfers$trip_id, '.x')
-    gtfs.x$transfers$stop_id <- paste0(gtfs.x$transfers$stop_id, '.x')
-  }
-  
-  if(!is_null(gtfs.y$transfers)){
-    gtfs.y$transfers$trip_id <- paste0(gtfs.y$transfers$trip_id, '.y')
-    gtfs.y$transfers$stop_id <- paste0(gtfs.y$transfers$stop_id, '.y')
-  }
   
   # bind rows ----
   gtfs <- list()
