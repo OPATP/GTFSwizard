e07932a009c3df616ddf921ad84d5d0bfe978ded
Gustavo Guimarães <49100132+gustavo234guimaraes@users.noreply.github.com>
2024-11-18 09:36:49 -0300

    correct data conversion

--- R/write_gtfs.R
+++ R/write_gtfs.R
@@ -107,10 +107,10 @@ as_df_ordinary <- function(df){
 
   df2 <- lapply(df, function(col){
     if(hms::is_hms(col)){
-      col = as.character(col)
+      col = as.character(col) %>% gsub('-','',x = .)
     }
-    if(is.POSIXct(col)|is.POSIXlt(col)|is.Date(col)){
-      col = as.character(col)
+    if(lubridate::is.POSIXct(col)|lubridate::is.POSIXlt(col)|lubridate::is.Date(col)){
+      col = as.character(col) %>% gsub('-','',x = .)
     }
     if(is.character(.col)){
       col[is.na(col)] <- ""
@@ -127,4 +127,6 @@ as_df_ordinary <- function(df){
 rm_servicepattern <- function(gtfs){
   gtfs[names(gtfs) != "dates_services"]
 }
+
+write_gtfs(wiz.read)
 
