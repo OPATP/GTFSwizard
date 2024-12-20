fc5575b18e624beca1f088ac499f273f6ed66b82
cgmobility <caiogcg.mobilidade@gmail.com>
2024-12-20 15:39:24 -0300

    upload get_transfers

---
+++ R/get_transfer_clusters.R
@@ -0,0 +1,61 @@
+
+
+get_transfer_clusters <- function(obj){
+  UseMethod('get_transfer_clusters')
+}
+
+get_transfer_clusters.wizardgtfs <- function(obj){
+  transfers <- get_high_transfer_stops(obj)
+  get_transfer_clusters(transfers)
+}
+
+
+get_transfer_clusters.wzd_transfers <- function(obj){
+  transfers_lst <- obj %>%
+    dplyr::group_by(service_pattern,hour) %>%
+    dplyr::group_split()
+
+  transfer_clusters <- lapply(transfers_lst, function(tran){
+
+    if(nrow(tran)>4){
+
+      tran_low <- tran %>%
+        dplyr::filter(n_routes<=quantile(n_routes,.75)) %>%
+        dplyr::mutate(cluster=1)
+
+      tran <- tran %>%
+        dplyr::filter(n_routes>quantile(n_routes,.75))
+
+      tran$cluster <- stats::kmeans(tran$n_routes,centers = 4)$cluster
+
+      tran %>%
+        dplyr::group_by(cluster) %>%
+        dplyr::mutate(mean_n_routes = mean(n_routes,na.rm = T)) %>%
+        dplyr::ungroup() %>%
+        dplyr::arrange(mean_n_routes) %>%
+        dplyr::group_by(mean_n_routes) %>%
+        dplyr::mutate(cluster = dplyr::cur_group_id()) %>%
+        dplyr::bind_rows(tran_low) %>%
+        dplyr::group_by(cluster) %>%
+        dplyr::mutate(mean_n_routes = mean(n_routes,na.rm = T)) %>%
+        return()
+    }else{
+      tran %>%
+        dplyr::arrange(n_routes) %>%
+        dplyr::mutate(cluster = 1:dplyr::n()) %>%
+        dplyr::group_by(cluster) %>%
+        dplyr::mutate(mean_n_routes = mean(n_routes,na.rm = T)) %>%
+        return()
+    }
+
+
+
+
+  }) %>% data.table::rbindlist()
+
+  class(transfer_clusters) <- c('wzd_transfercluster',class(transfer_clusters))
+
+  attr(transfer_clusters,'stop_position') <- attr(obj,'stop_position')
+
+  return(transfer_clusters)
+}
---
+++ R/plot_transfer_clusters.R
@@ -0,0 +1,49 @@
+
+
+plot.wzd_transfercluster <- function(x,hour){
+
+  if(missing('hour')){
+    message("Filtering the time with the most trips")
+    hour <- x$hour[which.max(x$n_trips)] %>% as.numeric()
+  }
+  checkmate::assert_numeric(hour)
+
+  h <- hour
+  x %>%
+    dplyr::filter(hour == h) %>%
+    dplyr::left_join(attr(x,'stop_position'),by = 'stop_id') %>%
+    dplyr::mutate(cluster = as.character(cluster)) %>%
+    sf::st_as_sf(coords = c('stop_lon','stop_lat'),crs = 4326) %>%
+    ggplot2::ggplot(ggplot2::aes(color = cluster,alpha = cluster))+
+    ggplot2::geom_sf()+
+    ggplot2::theme_void()+
+    ggplot2::facet_wrap(~service_pattern)+
+    ggplot2::scale_alpha_manual(
+      breaks = as.character(1:4),
+      values = c(0,0.1,.8,1)
+    ) +
+    ggplot2::guides(alpha = 'none')+
+    ggplot2::labs(title = 'Clusters oh high transfers',subtitle = paste0('Hour: ',hour))
+}
+
+plot(transfer_clusters)
+
+
+transfer_clusters %>%
+  dplyr::filter(hour == 6) %>%
+  dplyr::filter(service_pattern == 'servicepattern-1') %>%
+  dplyr::filter(cluster == 4|stop_id=='5819') %>%
+  dplyr::left_join(attr(transfer_clusters,'stop_position'),by = 'stop_id') %>%
+  sf::st_as_sf(coords = c('stop_lon','stop_lat'),crs = 4326) %>%
+  leaflet::leaflet() %>%
+  leaflet::addMarkers(label =  ~n_routes) %>%
+  leaflet::addTiles()
+
+
+gtfs$stops %>%
+  dplyr::filter(stringr::str_detect(stop_name,'TERMINAL')) %>%
+  sf::st_as_sf(coords = c('stop_lon','stop_lat'),crs = 4326) %>%
+  leaflet::leaflet() %>%
+  leaflet::addMarkers() %>%
+  leaflet::addTiles()
+
